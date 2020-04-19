c6ad31e14e25a97406edfe6bb879ed462955d810
steven <bbtrombone@sbcglobal.net>
2020-04-19 18:03:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,5 +27,8 @@ def return_string_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-    
-    
+    new_array.unshift(array[counter]
+    counter = counter + 1
+  end 
+  puts new_array
+end
