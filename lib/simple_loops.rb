d473897a2da8feda42a549b9b44d611ea4a80a7c
steven <bbtrombone@sbcglobal.net>
2020-04-19 17:54:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,4 +7,10 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_n_times
+def loop_message_n_times(string,3)
+  counter = 0
+  while counter < 3 do
+    puts(string)
+    counter = counter + 1
+  end
+end
